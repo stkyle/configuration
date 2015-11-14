43eedaa52d0ca0ae18c3e760bab1d5324b5ea3b2
stkyle <stkyle@users.noreply.github.com>
2015-11-14 12:30:23 -0500

    Update test_configparser.py

--- test_configparser.py
+++ test_configparser.py
@@ -3,3 +3,32 @@
 """
 
 """
+import ConfigParser
+
+sample_config = """
+[DEFAULT]
+# Comment A
+
+[SectionOne]
+Status: Single
+Name: Derek
+Value: Yes
+
+# Comment B
+# Comment C
+Age: 30
+Single: True
+
+[SectionTwo]
+FavoriteColor=Green
+[SectionThree]
+FamilyName: Johnson
+
+[Others]
+Route: 66
+"""
+
+
+config = ConfigParser.ConfigParser()
+config.readfp(sample_config)
+print(config.sections())
